70946c22a9c2e201697a96ba3344a5bcd3abd57e
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2023-11-01 18:58:44 +0100

    Add functions to apply action effects

--- genfond/state_space_generator.py
+++ genfond/state_space_generator.py
@@ -1,6 +1,8 @@
-from pddl.logic.base import And, Not
+from pddl.logic.base import And, Not, OneOf
 from pddl.logic import Predicate
 from pddl.logic.predicates import EqualTo
+from collections.abc import Collection, Set
+from pddl.logic.effects import AndEffect, When
 
 
 def check_formula(state, formula):
@@ -16,3 +18,39 @@ def check_formula(state, formula):
         return formula.left == formula.right
     else:
         raise ValueError('Unknown formula type: {}'.format(type(formula)))
+
+
+def apply_action_effects(state, action):
+    if isinstance(action.effect, OneOf):
+        return {
+            apply_effects(state, effect)
+            for effect in action.effect.operands
+        }
+    else:
+        return {apply_effects(state, action.effect)}
+
+
+def apply_effects(state, effects):
+    if isinstance(effects, Collection):
+        for effect in effects:
+            state = apply_effects(state, effect)
+        return state
+    elif isinstance(effects, And):
+        for effect in effects.operands:
+            state = apply_effects(state, effect)
+        return state
+    elif isinstance(effects, AndEffect):
+        for effect in effects.operands:
+            state = apply_effects(state, effect)
+        return state
+    elif isinstance(effects, Predicate):
+        return frozenset(state | {effects})
+    elif isinstance(effects, Not):
+        return frozenset([f for f in state if f != effects.argument])
+    elif isinstance(effects, When):
+        if check_formula(state, effects.condition):
+            return apply_effects(state, effects.effect)
+        else:
+            return state
+    else:
+        raise ValueError('Unknown effect type: {}'.format(type(effects)))
---
+++ tests/test_apply_effects.py
@@ -0,0 +1,51 @@
+from genfond.state_space_generator import apply_action_effects
+from helpers import get_action
+from genfond.ground import ground
+from pddl.logic import constants, variables, Predicate
+
+
+def test_apply_effects_simple_blocks(simple_blocks):
+    domain, problem = simple_blocks
+    ground_actions = ground(domain, problem)
+    a, b = constants('a b')
+    holding = Predicate('holding', a)
+    states = apply_action_effects(problem.init,
+                                  get_action(ground_actions, 'pick', (a, b)))
+    assert states == frozenset([frozenset([holding(a)])])
+    states = apply_action_effects(next(iter(states)),
+                                  get_action(ground_actions, 'put', (a, b)))
+    assert states == {problem.init}
+
+
+def test_apply_effects_fond_blocks(fond_blocks):
+    domain, problem = fond_blocks
+    ground_actions = ground(domain, problem)
+    x, y = variables('x y')
+    a, b, c, table = constants('A B C Table')
+    on = Predicate('on', x, y)
+    clear = Predicate('clear', x)
+    block = Predicate('block', x)
+    blocks = set([block(a), block(b), block(c), block(table)])
+    states = apply_action_effects(
+        problem.init, get_action(ground_actions, 'puton', (c, b, a)))
+    assert len(states) == 2
+    assert states == {
+        frozenset(blocks | {
+            on(a, table),
+            clear(a),
+            on(b, table),
+            on(c, b),
+            clear(c),
+            clear(table)
+        }),
+        frozenset(
+            blocks | {
+                on(a, table),
+                clear(a),
+                on(b, table),
+                clear(b),
+                on(c, table),
+                clear(c),
+                clear(table)
+            })
+    }
