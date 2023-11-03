a0d3c9deefc2f9bde36887b42f900bb3a5aadd9e
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2023-11-03 07:57:53 +0100

    Add goal predicates to feature generation

--- genfond/feature_generator.py
+++ genfond/feature_generator.py
@@ -1,17 +1,31 @@
 from dlplan.core import VocabularyInfo, InstanceInfo, State, \
     SyntacticElementFactory
 import dlplan.generator as dlplan_gen
-from genfond.ground import ground_domain_predicates
+from pddl.logic import Predicate
+from .ground import ground_domain_predicates
+from .state_space_generator import apply_effects
 
 
 def construct_vocabulary_info(domain):
     vocabulary = VocabularyInfo()
     for predicate in domain.predicates:
+        assert f'{predicate.name}_G' not in [p.name for p in domain.predicates]
+    for predicate in domain.predicates:
         # TODO some predicates may be static.
         vocabulary.add_predicate(predicate.name, predicate.arity)
+        vocabulary.add_predicate(f'{predicate.name}_G', predicate.arity)
     for constant in domain.constants:
         vocabulary.add_constant(constant.name)
     return vocabulary
+
+
+def _get_state_from_goal(goal_formula):
+    state = apply_effects(set(), goal_formula)
+    goal_state = {
+        Predicate(f'{predicate.name}_G', *predicate.terms)
+        for predicate in state
+    }
+    return goal_state
 
 
 def construct_instance_info(vocabulary, domain, problem):
@@ -20,6 +34,9 @@ def construct_instance_info(vocabulary, domain, problem):
     for predicate in ground_domain_predicates(domain, problem):
         map[predicate] = instance.add_atom(predicate.name,
                                            [str(t) for t in predicate.terms])
+        goal_predicate = Predicate(f'{predicate.name}_G', *predicate.terms)
+        map[goal_predicate] = instance.add_atom(
+            goal_predicate.name, [str(t) for t in predicate.terms])
     return instance, map
 
 
@@ -30,8 +47,11 @@ class FeaturePool:
         instance, mapping = construct_instance_info(vocabulary, domain,
                                                     problem)
         factory = SyntacticElementFactory(vocabulary)
+        goal_state = _get_state_from_goal(problem.goal)
         self.states = {
-            state: State(instance, [mapping[predicate] for predicate in state])
+            state:
+            State(instance,
+                  [mapping[predicate] for predicate in state | goal_state])
             for state in pddl_states
         }
         str_features = dlplan_gen.generate_features(factory,
--- tests/test_feature_generator.py
+++ tests/test_feature_generator.py
@@ -1,6 +1,6 @@
 from genfond.feature_generator import FeaturePool
 from genfond.state_space_generator import generate_state_space, \
-        apply_action_effects
+        apply_action_effects, check_formula
 from genfond.ground import ground
 from pddl.logic import constants, Predicate
 from helpers import get_action
@@ -23,6 +23,11 @@ def test_generate_features_simple_blocks(simple_blocks):
                                          on_ab) == 0
     assert feature_pool.evaluate_feature('n_count(r_primitive(on,0,1))',
                                          on_ab) == 1
+    assert feature_pool.evaluate_feature('n_count(r_primitive(on_G,0,1))',
+                                         on_ab) == 1
+    assert feature_pool.evaluate_feature(
+        'n_count(c_all(r_primitive(on,0,1),'
+        'c_some(r_primitive(on_G,0,1),c_top)))', on_ab) == 2
     holding_a = frozenset([Predicate('holding', a)])
     assert feature_pool.evaluate_feature('b_empty(c_primitive(holding,0))',
                                          holding_a) is False
@@ -32,6 +37,11 @@ def test_generate_features_simple_blocks(simple_blocks):
                                          holding_a) == 1
     assert feature_pool.evaluate_feature('n_count(r_primitive(on,0,1))',
                                          holding_a) == 0
+    assert feature_pool.evaluate_feature('n_count(r_primitive(on_G,0,1))',
+                                         holding_a) == 1
+    assert feature_pool.evaluate_feature(
+        'n_count(c_all(r_primitive(on,0,1),'
+        'c_some(r_primitive(on_G,0,1),c_top)))', holding_a) == 2
 
 
 def test_generate_features_fond_blocks(fond_blocks):
@@ -39,6 +49,11 @@ def test_generate_features_fond_blocks(fond_blocks):
     ground_actions = ground(domain, problem)
     state_graph = generate_state_space(domain, problem)
     states = [node.state for node in state_graph.nodes.values()]
+    gstates = [
+        node.state for node in state_graph.nodes.values()
+        if check_formula(node.state, problem.goal)
+    ]
+    assert len(gstates) > 0
     feature_pool = FeaturePool(domain, problem, states)
     a, b, c, table = constants('A B C Table')
     istate = state_graph.root.state
@@ -48,6 +63,9 @@ def test_generate_features_fond_blocks(fond_blocks):
                                          istate) == 3
     assert feature_pool.evaluate_feature(
         'n_count(c_some(r_primitive(on,0,1),c_one_of(Table)))', istate) == 2
+    assert feature_pool.evaluate_feature(
+        'n_count(r_and(r_primitive(on,0,1),r_primitive(on_G,0,1)))',
+        istate) == 0
     cta_puton = get_action(ground_actions, 'puton', (c, table, a))
     cta_puton_state = next(iter(apply_action_effects(istate, cta_puton)))
     assert feature_pool.evaluate_feature('n_count(c_primitive(clear,0))',
@@ -55,3 +73,9 @@ def test_generate_features_fond_blocks(fond_blocks):
     assert feature_pool.evaluate_feature(
         'n_count(c_some(r_primitive(on,0,1),c_one_of(Table)))',
         cta_puton_state) == 3
+    assert feature_pool.evaluate_feature(
+        'n_count(r_and(r_primitive(on,0,1),r_primitive(on_G,0,1)))',
+        cta_puton_state) == 0
+    assert feature_pool.evaluate_feature(
+        'n_count(r_and(r_primitive(on,0,1),r_primitive(on_G,0,1)))',
+        gstates[0]) == 1
