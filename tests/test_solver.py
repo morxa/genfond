2bb6066c20307596797784d4ba12b5d96111ca7e
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2023-11-13 21:46:59 +0100

    Only distinguish transitions with boolean evaluations

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -5,15 +5,20 @@ def test_solver_choose_good_trans():
     program = """
         feature(f).
         feature_complexity(f, 1).
+        feature(g).
+        feature_complexity(g, 1).
 
         state(0, 0).
         eval(0, 0, f, 0).
+        eval(0, 0, g, 0).
 
         state(0, 1).
         eval(0, 1, f, 1).
+        eval(0, 1, g, 0).
 
         state(0, 2).
-        eval(0, 2, f, 2).
+        eval(0, 2, f, 1).
+        eval(0, 2, g, 1).
         goal(0, 2).
 
         trans(0, 0, a, 1).
@@ -22,6 +27,9 @@ def test_solver_choose_good_trans():
     solver = Solver(program)
     assert solver.solve()
     assert solver.solution['good_trans'] == {(0, 0, 2)}
+    assert solver.solution['selected'] == {'f', 'g'}
+    assert solver.solution['trans_delta'] == {(0, 0, 1, 'f', 1), (0, 0, 1, 'g', 0), (0, 0, 2, 'f', 1),
+                                              (0, 0, 2, 'g', 1)}
 
 
 def test_solver_no_good_trans():
@@ -50,22 +58,36 @@ def test_solver_two_conflicting_actions():
     program = """
         feature(f).
         feature_complexity(f, 1).
+        feature(g).
+        feature_complexity(g, 1).
+        feature(dead).
+        feature_complexity(dead, 1).
 
         state(0, 0).
         eval(0, 0, f, 0).
+        eval(0, 0, g, 0).
+        eval(0, 0, dead, 0).
 
         state(0, 1).
         eval(0, 1, f, 1).
+        eval(0, 1, g, 0).
+        eval(0, 1, dead, 0).
 
         state(0, 2).
         eval(0, 2, f, 2).
+        eval(0, 2, g, 0).
+        eval(0, 2, dead, 1).
 
         state(0, 3).
         eval(0, 3, f, 3).
+        eval(0, 3, g, 0).
+        eval(0, 3, dead, 0).
 
         state(0, 4).
         eval(0, 4, f, 4).
+        eval(0, 4, g, 1).
         goal(0, 4).
+        eval(0, 4, dead, 0).
 
         trans(0, 0, a, 1).
         trans(0, 0, a, 0).
@@ -76,8 +98,12 @@ def test_solver_two_conflicting_actions():
     """
     solver = Solver(program)
     assert solver.solve()
-    assert solver.solution['good_trans'] == {(0, 0, 0), (0, 0, 1), (0, 1, 3),
-                                             (0, 3, 4)}
+    assert solver.solution['good_trans'] == {(0, 0, 0), (0, 0, 1), (0, 1, 3), (0, 3, 4)}
+    assert solver.solution['selected'] == {'f', 'dead'}
+    assert solver.solution['trans_delta'] == {(0, 0, 0, 'f', 0), (0, 0, 0, 'dead', 0), (0, 0, 1, 'f', 1),
+                                              (0, 0, 1, 'dead', 0), (0, 0, 2, 'f', 1), (0, 0, 2, 'dead', 1),
+                                              (0, 1, 3, 'f', 1), (0, 1, 3, 'dead', 0), (0, 3, 4, 'f', 1),
+                                              (0, 3, 4, 'dead', 0)}
 
 
 def test_solver_feature_selection():
@@ -137,6 +163,8 @@ def test_solver_equiv():
     solver = Solver(program)
     assert solver.solve()
     assert solver.solution['selected'] == {'f'}
+    assert solver.solution['good_trans'] == {(0, 0, 1), (1, 0, 1)}
+    assert solver.solution['trans_delta'] == {(0, 0, 1, 'f', 1), (1, 0, 1, 'f', 1)}
 
 
 def test_solver_equiv2():
@@ -163,23 +191,23 @@ def test_solver_equiv2():
         eval(0, 2, h, 0).
 
         state(0, 3).
-        eval(0, 3, f, 3).
+        eval(0, 3, f, 5).
         eval(0, 3, g, 0).
         eval(0, 3, h, 0).
 
         state(0, 5).
-        eval(0, 5, f, 3).
+        eval(0, 5, f, 5).
         eval(0, 5, g, 0).
         eval(0, 5, h, 1).
 
         state(0, 4).
-        eval(0, 4, f, 4).
+        eval(0, 4, f, 8).
         eval(0, 4, g, 0).
         eval(0, 4, h, 0).
         goal(0, 4).
 
         state(0, 6).
-        eval(0, 6, f, 4).
+        eval(0, 6, f, 8).
         eval(0, 6, g, 0).
         eval(0, 6, h, 1).
         goal(0, 6).
@@ -198,9 +226,14 @@ def test_solver_equiv2():
     solver = Solver(program)
     assert solver.solve()
     assert solver.solution['selected'] == {'f', 'g'}
-    assert solver.solution['good_trans'] == {(0, 0, 0), (0, 0, 1), (0, 1, 3),
-                                             (0, 3, 1), (0, 3, 4), (0, 1, 5),
+    assert solver.solution['good_trans'] == {(0, 0, 0), (0, 0, 1), (0, 1, 3), (0, 3, 1), (0, 3, 4), (0, 1, 5),
                                              (0, 5, 1), (0, 5, 6)}
+    assert solver.solution['trans_delta'] == {(0, 0, 0, 'f', 0), (0, 0, 0, 'g', 0), (0, 0, 1, 'f', 1),
+                                              (0, 0, 1, 'g', 0), (0, 0, 2, 'f', 1), (0, 0, 2, 'g', 1),
+                                              (0, 1, 3, 'f', 1), (0, 1, 3, 'g', 0), (0, 1, 5, 'f', 1),
+                                              (0, 1, 5, 'g', 0), (0, 3, 1, 'f', -1), (0, 3, 1, 'g', 0),
+                                              (0, 5, 1, 'f', -1), (0, 5, 1, 'g', 0), (0, 3, 4, 'f', 1),
+                                              (0, 3, 4, 'g', 0), (0, 5, 6, 'f', 1), (0, 5, 6, 'g', 0)}
 
 
 def test_solver_rank():
@@ -233,3 +266,87 @@ def test_solver_rank():
     solver = Solver(program)
     assert solver.solve()
     assert (0, 0, 3) in solver.solution['good_trans']
+
+
+def test_bool_equiv():
+    program = """
+        feature(f).
+        feature_complexity(f, 1).
+        feature(g).
+        feature_complexity(g, 2).
+
+        state(0, 0).
+        eval(0, 0, f, 3).
+        eval(0, 0, g, 0).
+
+        state(0, 1).
+        eval(0, 1, f, 2).
+        eval(0, 1, g, 0).
+
+        state(0, 2).
+        eval(0, 2, f, 1).
+        eval(0, 2, g, 0).
+
+        state(0, 3).
+        eval(0, 3, f, 1).
+        eval(0, 3, g, 1).
+        goal(0, 3).
+
+        % This transition cannot be chosen because it cannot be
+        % boolean-distinguished from the next transition (which is a bad
+        % transition).
+        trans(0, 0, a, 1).
+        trans(0, 1, a, 2).
+        trans(0, 1, b, 3).
+    """
+    solver = Solver(program)
+    assert solver.solve() is False, f'Unexpected solution {solver.solution}'
+
+
+def test_bool_equiv2():
+    program = """
+        feature(f).
+        feature_complexity(f, 1).
+        feature(g).
+        feature_complexity(g, 2).
+        feature(h).
+        feature_complexity(h, 3).
+
+        state(0, 0).
+        eval(0, 0, f, 4).
+        eval(0, 0, g, 0).
+        eval(0, 0, h, 0).
+
+        state(0, 1).
+        eval(0, 1, f, 3).
+        eval(0, 1, g, 0).
+        eval(0, 1, h, 0).
+
+        state(0, 2).
+        eval(0, 2, f, 2).
+        eval(0, 2, g, 1).
+        eval(0, 2, h, 0).
+
+        state(0, 3).
+        eval(0, 3, f, 1).
+        eval(0, 3, g, 0).
+        eval(0, 3, h, 0).
+
+        state(0, 4).
+        eval(0, 4, f, 1).
+        eval(0, 4, g, 0).
+        eval(0, 4, h, 1).
+        goal(0, 4).
+
+        trans(0, 0, a, 1).
+        trans(0, 1, a, 2).
+        trans(0, 1, b, 3).
+        trans(0, 3, a, 4).
+    """
+    solver = Solver(program)
+    assert solver.solve()
+    # 'g' must also be chosen to bool-distinguish states 2 and 3.
+    assert solver.solution['selected'] == {'f', 'g', 'h'}
+    assert solver.solution['good_trans_delta'] == {(0, 0, 1, 'f', -1), (0, 0, 1, 'g', 0), (0, 0, 1, 'h', 0),
+                                                   (0, 1, 3, 'f', -1), (0, 1, 3, 'g', 0), (0, 1, 3, 'h', 0),
+                                                   (0, 3, 4, 'f', 0), (0, 3, 4, 'g', 0), (0, 3, 4, 'h', 1)}
