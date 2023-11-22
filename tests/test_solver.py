afd1dd79920064074382003759c6431aac25820b
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2023-11-22 15:24:11 +0100

    Goals must be bool-distinguishable

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -100,11 +100,12 @@ def test_solver_two_conflicting_actions():
     solver = Solver(program)
     assert solver.solve()
     assert solver.solution['good_trans'] == {(0, 0, 0), (0, 0, 1), (0, 1, 3), (0, 3, 4)}
-    assert solver.solution['selected'] == {'f', 'dead'}
-    assert solver.solution['trans_delta'] == {(0, 0, 0, 'f', 0), (0, 0, 0, 'dead', 0), (0, 0, 1, 'f', 1),
-                                              (0, 0, 1, 'dead', 0), (0, 0, 2, 'f', 1), (0, 0, 2, 'dead', 1),
-                                              (0, 1, 3, 'f', 1), (0, 1, 3, 'dead', 0), (0, 3, 4, 'f', 1),
-                                              (0, 3, 4, 'dead', 0)}
+    assert solver.solution['selected'] == {'f', 'g', 'dead'}
+    assert solver.solution['trans_delta'] == {(0, 0, 0, 'f', 0), (0, 0, 0, 'g', 0), (0, 0, 0, 'dead', 0),
+                                              (0, 0, 1, 'f', 1), (0, 0, 1, 'g', 0), (0, 0, 1, 'dead', 0),
+                                              (0, 0, 2, 'f', 1), (0, 0, 2, 'g', 0), (0, 0, 2, 'dead', 1),
+                                              (0, 1, 3, 'f', 1), (0, 1, 3, 'g', 0), (0, 1, 3, 'dead', 0),
+                                              (0, 3, 4, 'f', 1), (0, 3, 4, 'g', 1), (0, 3, 4, 'dead', 0)}
 
 
 def test_solver_feature_selection():
@@ -119,10 +120,10 @@ def test_solver_feature_selection():
 
         state(0, 1).
         eval(0, 1, f, 1).
-        eval(0, 1, g, 1).
+        eval(0, 1, g, 0).
         state(0, 2).
         eval(0, 2, f, 2).
-        eval(0, 2, g, 1).
+        eval(0, 2, g, 0).
         state(0, 3).
         eval(0, 3, f, 1).
         eval(0, 3, g, 2).
@@ -172,6 +173,7 @@ def test_solver_equiv2(program_with_nontriv_equiv):
     program = program_with_nontriv_equiv
     solver = Solver(program)
     assert solver.solve()
+    print(solver.solution['rank'])
     assert solver.solution['selected'] == {'n_f', 'b_g'}
 
 
@@ -179,17 +181,24 @@ def test_solver_rank():
     program = """
         feature(f).
         feature_complexity(f, 1).
+        feature(g).
+        feature_complexity(g, 1).
+
         state(0, 0).
         eval(0, 0, f, 0).
+        eval(0, 0, g, 0).
 
         state(0, 1).
         eval(0, 1, f, 1).
+        eval(0, 1, g, 0).
 
         state(0, 2).
         eval(0, 2, f, 2).
+        eval(0, 2, g, 0).
 
         state(0, 3).
         eval(0, 3, f, 3).
+        eval(0, 3, g, 1).
         goal(0, 3).
 
         trans(0, 0, b, 1).
