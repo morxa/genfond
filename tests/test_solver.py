87a1df1bddc244e9641efb3d8ec3176faa90c8e6
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2023-12-25 18:17:47 +0100

    Adapt solver tests to latest changes

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -30,9 +30,8 @@ def test_solver_choose_good_trans():
     solver = Solver(program)
     assert solver.solve()
     assert solver.solution['good_trans'] == {(0, 0, 2)}
-    assert solver.solution['selected'] == {'f', 'g'}
-    assert solver.solution['trans_delta'] == {(0, 0, 1, 'f', 1), (0, 0, 1, 'g', 0), (0, 0, 2, 'f', 1),
-                                              (0, 0, 2, 'g', 1)}
+    assert solver.solution['selected'] == {'g'}
+    assert solver.solution['trans_delta'] == {(0, 0, 1, 'g', 0), (0, 0, 2, 'g', 1)}
 
 
 def test_solver_two_conflicting_actions():
@@ -84,12 +83,11 @@ def test_solver_two_conflicting_actions():
     solver = Solver(program)
     assert solver.solve()
     assert solver.solution['good_trans'] == {(0, 0, 0), (0, 0, 1), (0, 1, 3), (0, 3, 4)}
-    assert solver.solution['selected'] == {'f', 'g', 'dead'}
-    assert solver.solution['trans_delta'] == {(0, 0, 0, 'f', 0), (0, 0, 0, 'g', 0), (0, 0, 0, 'dead', 0),
-                                              (0, 0, 1, 'f', 1), (0, 0, 1, 'g', 0), (0, 0, 1, 'dead', 0),
-                                              (0, 0, 2, 'f', 1), (0, 0, 2, 'g', 0), (0, 0, 2, 'dead', 1),
-                                              (0, 1, 3, 'f', 1), (0, 1, 3, 'g', 0), (0, 1, 3, 'dead', 0),
-                                              (0, 3, 4, 'f', 1), (0, 3, 4, 'g', 1), (0, 3, 4, 'dead', 0)}
+    assert solver.solution['selected'] == {'f', 'g'}
+    assert solver.solution['trans_delta'] == {(0, 0, 0, 'f', 0), (0, 0, 0, 'g', 0), (0, 0, 1, 'f', 1), (0, 0, 1, 'g',
+                                                                                                        0),
+                                              (0, 0, 2, 'f', 1), (0, 0, 2, 'g', 0), (0, 1, 3, 'f', 1),
+                                              (0, 1, 3, 'g', 0), (0, 3, 4, 'f', 1), (0, 3, 4, 'g', 1)}
 
 
 def test_solver_feature_selection():
@@ -162,8 +160,7 @@ def test_solver_equiv2(program_with_nontriv_equiv):
     program = program_with_nontriv_equiv
     solver = Solver(program)
     assert solver.solve()
-    print(solver.solution['rank'])
-    assert solver.solution['selected'] == {'n_f', 'b_g'}
+    assert solver.solution['selected'] == {'b_g'}
 
 
 def test_solver_rank():
@@ -251,26 +248,23 @@ def test_solver_bool_equiv2(simple_program):
     solver = Solver(simple_program)
     assert solver.solve()
     # 'g' must also be chosen to bool-distinguish states 2 and 3.
-    assert solver.solution['selected'] == {'n_f', 'b_g', 'b_h'}
+    assert solver.solution['selected'] == {'b_g', 'b_h'}
     assert solver.solution['bool_repr'] == {(0, 0), (0, 2), (0, 4)}
 
 
 def test_solver_generate_policy(simple_program):
     solver = Solver(simple_program)
     assert solver.solve()
-    assert solver.solution['good_trans_delta'] == {(0, 0, 1, 'n_f', -1), (0, 0, 1, 'b_g', 0), (0, 0, 1, 'b_h', 0),
-                                                   (0, 1, 3, 'n_f', -1), (0, 1, 3, 'b_g', 0), (0, 1, 3, 'b_h', 0),
-                                                   (0, 3, 4, 'n_f', 0), (0, 3, 4, 'b_g', 0), (0, 3, 4, 'b_h', 1)}
+    assert solver.solution['good_trans_delta'] == {(0, 0, 1, 'b_g', 0), (0, 0, 1, 'b_h', 0), (0, 1, 3, 'b_g', 0),
+                                                   (0, 1, 3, 'b_h', 0), (0, 3, 4, 'b_g', 0), (0, 3, 4, 'b_h', 1)}
     policy = generate_policy(solver.solution)
     print(policy)
     assert policy.rules == {
         PolicyRule({
-            'n_f': Cond.POSITIVE,
             'b_g': Cond.FALSE,
             'b_h': Cond.FALSE
-        }, [[('n_f', Effect.DECREASE)]]),
+        }, [[]]),
         PolicyRule({
-            'n_f': Cond.POSITIVE,
             'b_g': Cond.FALSE,
             'b_h': Cond.FALSE
         }, [[('b_h', Effect.SET)]])
@@ -283,29 +277,16 @@ def test_solver_policy_nontriv_equiv(program_with_nontriv_equiv):
     assert solver.solution['good_trans'] == {(0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 1, 'g1'), (0, 2, 'g2'), (0, 1, 0),
                                              (0, 2, 0)}
     assert solver.solution['trans_delta'] == {
-        (0, 0, 0, 'n_f', 0),
         (0, 0, 0, 'b_g', 0),
-        (0, 0, 1, 'n_f', 1),
         (0, 0, 1, 'b_g', 0),
-        (0, 0, 2, 'n_f', 1),
         (0, 0, 2, 'b_g', 0),
-        (0, 1, 0, 'n_f', -1),
         (0, 1, 0, 'b_g', 0),
-        (0, 2, 0, 'n_f', -1),
         (0, 2, 0, 'b_g', 0),
-        (0, 1, 'g1', 'n_f', 0),
         (0, 1, 'g1', 'b_g', 1),
-        (0, 2, 'g2', 'n_f', 0),
         (0, 2, 'g2', 'b_g', 1),
     }
     policy = generate_policy(solver.solution)
     assert policy.rules == {
-        PolicyRule({
-            'n_f': Cond.ZERO,
-            'b_g': Cond.FALSE
-        }, [[], [('n_f', Effect.INCREASE)]]),
-        PolicyRule({
-            'n_f': Cond.POSITIVE,
-            'b_g': Cond.FALSE
-        }, [[('n_f', Effect.DECREASE)], [('b_g', Effect.SET)]]),
+        PolicyRule({'b_g': Cond.FALSE}, [[]]),
+        PolicyRule({'b_g': Cond.FALSE}, [[], [('b_g', Effect.SET)]]),
     }
