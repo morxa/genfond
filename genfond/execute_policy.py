7cd86121c4e2d7ae7dc5b33f5d5390e7308d18da
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-06-10 19:28:41 +0200

    Allow `max_steps` param in `execute_policy`

--- genfond/execute_policy.py
+++ genfond/execute_policy.py
@@ -3,8 +3,8 @@ from .execute_rule_policy import execute_rule_policy
 from .execute_datalog_policy import execute_datalog_policy
 
 
-def execute_policy(domain, problem, policy):
+def execute_policy(domain, problem, policy, max_steps=0):
     if isinstance(policy, DatalogPolicy):
-        return execute_datalog_policy(domain, problem, policy)
+        return execute_datalog_policy(domain, problem, policy, max_steps)
     else:
-        return execute_rule_policy(domain, problem, policy)
+        return execute_rule_policy(domain, problem, policy, max_steps)
