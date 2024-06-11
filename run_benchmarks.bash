267346c1e358823a4e425fad535efad40e925695
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-06-11 16:14:40 +0200

    Adapt run_benchmarks to datalog policies

--- run_benchmarks.bash
+++ run_benchmarks.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DOMAINS="${DOMAINS:-$(ls domains/non-deterministic/)}"
-CONSTRAINTS="${CONSTRAINTS:-state trans}"
+DOMAINS="${DOMAINS:-$(find domains/non-deterministic -mindepth 1 -maxdepth 1 -type d)}"
+POLICY_TYPE="${POLICY_TYPE:-datalog}"
 
 STAMP="$(date -Iseconds)"
 RESDIR="results-$STAMP"
 mkdir -p "$RESDIR"
 
 for domain in $DOMAINS; do
-  for constraint in $CONSTRAINTS; do
-    sbatch --exclude='cn-[409-415]' -J $domain-$constraint genfond.bash python genfond.py --name $domain -n 32 --max-memory 240000 --constraints $constraint --policy-steps 10000 --policy-iterations 10 --max-complexity 15 --dump-failed-policies -o $RESDIR/$domain-$constraint.policy --stats $RESDIR/stats.csv domains/non-deterministic/$domain/{domain.pddl,p*}
+  for ptype in $POLICY_TYPE; do
+    sbatch --exclude='cn-[409-415]' -J $domain-$ptype genfond.bash python genfond.py --name $domain -n 32 --max-memory 240000 --type $ptype --policy-steps 10000 --policy-iterations 10 --max-complexity 15 --dump-failed-policies -o $RESDIR/$domain-$constraint.policy --stats $RESDIR/stats.csv $domain/{domain.pddl,p*}
   done
 done
