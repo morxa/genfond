1b9908df73b3a0ab562a5e7f1f9466aaaa107ae6
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-06-11 12:05:46 +0200

    Add state constraints to datalog policy generation

--- genfond/generate_datalog_policy.py
+++ genfond/generate_datalog_policy.py
@@ -1,5 +1,8 @@
 import re
-from genfond.datalog_policy import DatalogPolicyRule, DatalogPolicy
+from genfond.datalog_policy import DatalogPolicyRule, DatalogPolicy, Cond
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def generate_datalog_policy(solution):
@@ -13,15 +16,44 @@ def generate_datalog_policy(solution):
         name = match.groups()[0]
         parameters = match.groups()[1].replace(' ', '').split(',')
         arg_to_var = dict()
-        vars = ['X', 'Y', 'Z']
+        # TODO do not hardcode the number of variables
+        vars = ['P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         for i, parameter in enumerate(parameters):
             if parameter not in arg_to_var:
                 arg_to_var[i + 1] = vars.pop(0)
         args_to_vars[(instance, state, action)] = arg_to_var
-    print(args_to_vars)
+    bool_eval_dict = dict()
+    for i, s, f, v in solution['bool_eval']:
+        bool_eval_dict[(i, s, f)] = v
     rules = []
+    dist_features = dict()
+    for instance, state, _, _, feature in solution.get('f_distinguished', []):
+        dist_features.setdefault((instance, state), []).append(feature)
+    state_conds = dict()
     for instance, state, action in solution['good_action']:
         conds[(instance, state, action)] = []
+        state_cond = dict()
+        for f in dist_features.get((instance, state), []):
+            v = bool_eval_dict[(instance, state, f)]
+            log.debug(f'Adding condition {f}={v}')
+            if f.startswith('b_'):
+                if v == 1:
+                    state_cond[f] = Cond.TRUE
+                elif v == 0:
+                    state_cond[f] = Cond.FALSE
+                else:
+                    raise ValueError(f'Unknown value {v}')
+            elif f.startswith('n_'):
+                if v == 1:
+                    state_cond[f] = Cond.POSITIVE
+                elif v == 0:
+                    state_cond[f] = Cond.ZERO
+                else:
+                    raise ValueError(f'Unknown value {v}')
+            else:
+                raise ValueError(f'Unknown value {v}')
+        state_conds[(instance, state, action)] = state_cond
+    log.debug(f'state_conds: {state_conds}')
     for instance, state, action, _, _, _, concept, pos, argnum in solution.get('c_distinguished', []):
         action = action.strip('"')
         concept = concept.strip('"')
@@ -42,6 +74,6 @@ def generate_datalog_policy(solution):
         action_name = match.groups()[0]
         args = ",".join(args_to_vars[key].values())
         action = f'{action_name}({args})'
-        rule = DatalogPolicyRule(action, body_conds)
+        rule = DatalogPolicyRule(action, body_conds, state_conds[key])
         rules.append(rule)
     return DatalogPolicy(rules, cost=solution['cost'])
--- tests/test_generate_datalog_policy.py
+++ tests/test_generate_datalog_policy.py
@@ -1,7 +1,7 @@
 from genfond.generate_datalog_policy import generate_datalog_policy
 from genfond.execute_datalog_policy import execute_datalog_policy
 from genfond.solver import Solver
-from genfond.datalog_policy import DatalogPolicy, DatalogPolicyRule
+from genfond.datalog_policy import DatalogPolicy, DatalogPolicyRule, Cond
 
 
 def test_generate_datalog_policy():
@@ -10,7 +10,7 @@ def test_generate_datalog_policy():
     # goal: on(C, D)
     solution = {
         'cost': 5,
-        'selected': {
+        'c_selected': {
             'c_one_of(Table)',
             'c_some(r_transitive_reflexive_closure(r_primitive(on,0,1)),c_projection(r_primitive(on_G,0,1),0))',
             'c_some(r_transitive_reflexive_closure(r_primitive(on,0,1)),c_projection(r_primitive(on_G,0,1),1))',
@@ -21,7 +21,7 @@ def test_generate_datalog_policy():
             (0, 1, 'puton(B, Table, D)'),
             (0, 2, 'puton(C, D, Table)'),
         },
-        'distinguished': {
+        'c_distinguished': {
             (0, 0, 'puton(A, Table, C)', 0, 0, 'puton(A, B, C)', 'c_one_of(Table)', 'pos', 2),
             (0, 0, 'puton(A, Table, C)', 0, 0, 'puton(A, E, C)', 'c_one_of(Table)', 'pos', 2),
             (0, 0, 'puton(A, Table, C)', 0, 0, 'puton(E, Table, F)',
@@ -52,4 +52,38 @@ def test_generate_datalog_policy():
             ('X', 'c_projection(r_primitive(on_G,0,1),0)'),
             ('Y', 'c_projection(r_primitive(on_G,0,1),1)'),
         ]),
+    ])
+
+
+def test_generate_datalog_policy_with_conds():
+    solution = {
+        'cost': 2,
+        'f_selected': {
+            'b_nullary(hold-key)',
+        },
+        'good_action': {
+            (0, 0, 'pick-key(k1)'),
+            (0, 1, 'move-forward-door-open(l1, l2, d1, d2)'),
+            (0, 2, 'move-forward-door-open(l2, l3, d2, d3)'),
+            (0, 3, 'move-forward-last-door-closed(l3, l4, d3)'),
+        },
+        'f_distinguished': {
+            (0, 0, 0, 1, 'b_nullary(hold-key)'),
+            (0, 1, 0, 0, 'b_nullary(hold-key)'),
+            (0, 2, 0, 0, 'b_nullary(hold-key)'),
+            (0, 3, 0, 0, 'b_nullary(hold-key)'),
+        },
+        'bool_eval': {
+            (0, 0, 'b_nullary(hold-key)', 0),
+            (0, 1, 'b_nullary(hold-key)', 1),
+            (0, 2, 'b_nullary(hold-key)', 1),
+            (0, 3, 'b_nullary(hold-key)', 1),
+        },
+    }
+    policy = generate_datalog_policy(solution)
+    print(policy)
+    assert policy == DatalogPolicy([
+        DatalogPolicyRule('move-forward-door-open(P1, P2, P3, P4)', [], {'b_nullary(hold-key)': Cond.TRUE}),
+        DatalogPolicyRule('move-forward-last-door-closed(P1, P2, P3)', [], {'b_nullary(hold-key)': Cond.TRUE}),
+        DatalogPolicyRule('pick-key(X)', [], {'b_nullary(hold-key)': Cond.FALSE}),
     ])
