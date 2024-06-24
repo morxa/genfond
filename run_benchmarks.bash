d27eefeda86caa728da4c981b449e4bd26a6278b
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-06-24 15:45:54 +0200

    Fix domain name in run_benchmarks

--- run_benchmarks.bash
+++ run_benchmarks.bash
@@ -8,7 +8,8 @@ RESDIR="results-$STAMP"
 mkdir -p "$RESDIR"
 
 for domain in $DOMAINS; do
+  domainname=$(basename $domain)
   for ptype in $POLICY_TYPE; do
-    sbatch --exclude='cn-[409-415]' -J $domain-$ptype genfond.bash python genfond.py --name $domain -n 32 --max-memory 240000 --type $ptype --policy-steps 10000 --policy-iterations 10 --max-complexity 15 --dump-failed-policies -o $RESDIR/$domain-$constraint.policy --stats $RESDIR/stats.csv $domain/{domain.pddl,p*}
+    sbatch --exclude='cn-[409-415]' -J $domainname-$ptype genfond.bash python genfond.py --name $domainname -n 32 --max-memory 240000 --type $ptype --policy-steps 10000 --policy-iterations 10 --max-complexity 15 --dump-failed-policies -o $RESDIR/$domainname-$constraint.policy --stats $RESDIR/stats.csv $domain/{domain.pddl,p*}
   done
 done
