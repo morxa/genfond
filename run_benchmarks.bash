55c180b2ca6b1cdca6bb9fb28e3f72b7dd19fa8a
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-06-24 15:46:05 +0200

    Allow setting exclude node flag on commandline

--- run_benchmarks.bash
+++ run_benchmarks.bash
@@ -2,6 +2,9 @@
 
 DOMAINS="${DOMAINS:-$(find domains/non-deterministic -mindepth 1 -maxdepth 1 -type d)}"
 POLICY_TYPE="${POLICY_TYPE:-datalog}"
+# Set EXCLUDE if some nodes should be excluded from the slurm job, e.g., `EXCLUDE="cn-[409-415]"`
+EXCLUDE="${EXCLUDE:+--exclude=$EXCLUDE}"
+
 
 STAMP="$(date -Iseconds)"
 RESDIR="results-$STAMP"
@@ -10,6 +13,6 @@ mkdir -p "$RESDIR"
 for domain in $DOMAINS; do
   domainname=$(basename $domain)
   for ptype in $POLICY_TYPE; do
-    sbatch --exclude='cn-[409-415]' -J $domainname-$ptype genfond.bash python genfond.py --name $domainname -n 32 --max-memory 240000 --type $ptype --policy-steps 10000 --policy-iterations 10 --max-complexity 15 --dump-failed-policies -o $RESDIR/$domainname-$constraint.policy --stats $RESDIR/stats.csv $domain/{domain.pddl,p*}
+    sbatch $EXCLUDE -J $domainname-$ptype genfond.bash python genfond.py --name $domainname -n 32 --max-memory 240000 --type $ptype --policy-steps 10000 --policy-iterations 10 --max-complexity 15 --dump-failed-policies -o $RESDIR/$domainname-$constraint.policy --stats $RESDIR/stats.csv $domain/{domain.pddl,p*}
   done
 done
