2778639afdde4aed2ca164b6eb8e051942584f2a
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-07-04 18:08:58 +0200

    Refactor `ground_action` into a separate function

--- genfond/ground.py
+++ genfond/ground.py
@@ -47,19 +47,25 @@ def _check_types(constant, variable, type_dict):
     return any(_is_subtype(constant.type_tag, v_type, type_dict) for v_type in variable.type_tags)
 
 
+def ground_action(domain, problem, action, grounding):
+    if not isinstance(action, Action):
+        action = [a for a in domain.actions if a.name == action][0]
+    mapping = dict(zip(action.parameters, grounding))
+    if not all(_check_types(c, v, domain.types) for v, c in mapping.items()):
+        return None
+    ground_precondition = _ground_ops(action.precondition, mapping)
+    ground_effect = _ground_ops(action.effect, mapping)
+    return Action(action.name, parameters=grounding, precondition=ground_precondition, effect=ground_effect)
+
+
 def ground(domain, problem):
     constants = domain.constants | problem.objects
     operators = []
     for action in domain.actions:
         for grounding in itertools.product(constants, repeat=len(action.parameters)):
-            mapping = dict(zip(action.parameters, grounding))
-            if not all(_check_types(c, v, domain.types) for v, c in mapping.items()):
-                continue
-            parameters = grounding
-            ground_precondition = _ground_ops(action.precondition, mapping)
-            ground_effect = _ground_ops(action.effect, mapping)
-            op = Action(action.name, parameters=parameters, precondition=ground_precondition, effect=ground_effect)
-            operators.append(op)
+            op = ground_action(domain, problem, action, grounding)
+            if op:
+                operators.append(op)
     return operators
 
 
