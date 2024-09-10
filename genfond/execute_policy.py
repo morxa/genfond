34b9af50f980c511bc1f07b4f2d26d617a361246
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-09-10 16:45:23 +0200

    Adapt policy executor to config handler

--- genfond/execute_policy.py
+++ genfond/execute_policy.py
@@ -1,10 +1,15 @@
 from .datalog_policy import DatalogPolicy
 from .execute_rule_policy import execute_rule_policy
 from .execute_datalog_policy import execute_datalog_policy
+from .config_handler import ConfigHandler
 
 
-def execute_policy(domain, problem, policy, config):
+def execute_policy(domain, problem, policy, config=None):
     if isinstance(policy, DatalogPolicy):
+        if not config:
+            config = ConfigHandler(type='datalog')
         return execute_datalog_policy(domain, problem, policy, config)
     else:
+        if not config:
+            config = ConfigHandler()
         return execute_rule_policy(domain, problem, policy, config)
