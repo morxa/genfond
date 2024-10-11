0b7f14b6f7b9206ea5b2eb0ae19147df2c449775
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-10-11 09:47:52 +0200

    Test learned policy only once on each problem

--- run_benchmarks.bash
+++ run_benchmarks.bash
@@ -16,6 +16,6 @@ for domain in $DOMAINS; do
   domainfile="$domain/domain.pddl"
   problemfiles=$(find $domain ! -name domain.pddl -name '*.pddl')
   for ptype in $POLICY_TYPE; do
-    sbatch $EXCLUDE -J $domainname-$ptype -o $RESDIR/out/%x-%j.out genfond.bash python -m genfond $VERBOSE --name $domainname -n 32 --max-memory 240000 --type $ptype --policy-steps 10000 --policy-iterations 10 --max-complexity 15 --dump-failed-policies -o $RESDIR/$domainname-$ptype.policy --stats $RESDIR/stats.csv $domainfile $problemfiles
+    sbatch $EXCLUDE -J $domainname-$ptype -o $RESDIR/out/%x-%j.out genfond.bash python -m genfond $VERBOSE --name $domainname -n 32 --max-memory 240000 --type $ptype --policy-steps 10000 --policy-iterations 1 --max-complexity 15 --dump-failed-policies -o $RESDIR/$domainname-$ptype.policy --stats $RESDIR/stats.csv $domainfile $problemfiles
   done
 done
