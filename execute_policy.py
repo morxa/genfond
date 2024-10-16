70fc953029c7f14d9d5d79102e2e1157d075f54f
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-10-16 14:26:52 +0200

    Add config parameter to execution script

--- execute_policy.py
+++ execute_policy.py
@@ -1,8 +1,10 @@
 import argparse
 from genfond.execute_policy import execute_policy
+from genfond.datalog_policy import DatalogPolicy
 import pddl
 import pickle
 import logging
+from genfond.config_handler import ConfigHandler
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -14,14 +16,17 @@ def main():
     parser.add_argument('problem', help='problem file')
     parser.add_argument('policy', help='policy file')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
+    parser.add_argument('--config', type=argparse.FileType('r'), help='config file')
     args = parser.parse_args()
     if args.verbose:
         logging.getLogger().setLevel(logging.DEBUG)
-    domain = pddl.parse_domain(args.domain)
-    problem = pddl.parse_problem(args.problem)
     with open(args.policy, 'rb') as f:
         policy = pickle.load(f)
-    actions_taken = execute_policy(domain, problem, policy)
+    type = 'datalog' if isinstance(policy, DatalogPolicy) else 'rule'
+    config = ConfigHandler(args.config, type, vars(args))
+    domain = pddl.parse_domain(args.domain)
+    problem = pddl.parse_problem(args.problem)
+    actions_taken = execute_policy(domain, problem, policy, config)
     log.info(f'{len(actions_taken)} actions taken: {", ".join([str(a) for a in actions_taken])}')
 
 
