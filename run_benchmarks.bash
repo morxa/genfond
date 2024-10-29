18292c309a322008fb303e5d363e565d6c85b1a3
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-10-29 18:29:20 +0100

    Run selected benchmarks by default

--- run_benchmarks.bash
+++ run_benchmarks.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOMAINS="${DOMAINS:-$(find domains/deterministic domains/non-deterministic -mindepth 1 -maxdepth 1 -type d)}"
+DOMAINS="${DOMAINS:-$(find -L domains/selected -mindepth 1 -maxdepth 1 -type d)}"
 POLICY_TYPE="${POLICY_TYPE:-datalog}"
 # Set EXCLUDE if some nodes should be excluded from the slurm job, e.g., `EXCLUDE="cn-[409-415]"`
 EXCLUDE="${EXCLUDE:+--exclude=$EXCLUDE}"
@@ -14,7 +14,7 @@ mkdir -p "$RESDIR/out"
 for domain in $DOMAINS; do
   domainname=$(basename $domain)
   domainfile="$domain/domain.pddl"
-  problemfiles=$(find $domain ! -name domain.pddl -name '*.pddl')
+  problemfiles=$(find -L $domain ! -name domain.pddl -name '*.pddl')
   for ptype in $POLICY_TYPE; do
     sbatch $EXCLUDE -J $domainname-$ptype -o $RESDIR/out/%x-%j.out genfond.bash python -m genfond $VERBOSE --name $domainname -n 32 --max-memory 240000 --type $ptype $CONFIG --dump-failed-policies --dump-config $RESDIR/$domainname-$ptype.yaml -o $RESDIR/$domainname-$ptype.policy --stats $RESDIR/stats.csv $domainfile $problemfiles
   done
