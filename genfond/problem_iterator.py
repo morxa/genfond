0e098e1ba2938658a34b3797843d8d785010b910
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-11-12 12:12:58 +0100

    Switch back to previous successful complexity when adding new problem

--- genfond/problem_iterator.py
+++ genfond/problem_iterator.py
@@ -21,15 +21,16 @@ class ProblemIterator:
         self.min_complexity = min_complexity
         self.max_complexity = max_complexity
         self.use_all_features = use_all_features
-        self.last_result = Result.SUCCESS
-        self.active_problems_solved = True
-        self.max_cost = MAX_COST
-        self.solved = {problem.name: False for problem in problems}
 
     def __iter__(self):
         self.active_problems = []
         self.all_features = False
         self.complexity = self.min_complexity
+        self.last_result = Result.SUCCESS
+        self.succ_complexity = self.complexity
+        self.active_problems_solved = True
+        self.max_cost = MAX_COST
+        self.solved = {problem.name: False for problem in self.problems}
         return self
 
     def set_last_result(self, result, cost=MAX_COST):
@@ -37,6 +38,7 @@ class ProblemIterator:
         if result == Result.SUCCESS:
             self.active_problems_solved = True
             self.max_cost = cost - 1
+            self.succ_complexity = self.complexity
             self.solved = {
                 problem.name: True if problem in self.active_problems else False
                 for problem in self.problems
@@ -53,14 +55,15 @@ class ProblemIterator:
         if (self.active_problems and self.last_result != Result.OUT_OF_RESOURCES and not self.all_features
                 and self.use_all_features):
             self.all_features = True
-        elif (self.active_problems and self.last_result != Result.OUT_OF_RESOURCES
-              and self.complexity < self.max_complexity and self.max_cost > self.complexity):
+        elif (self.active_problems and self.all_features and self.complexity < self.max_complexity
+              and self.max_cost > self.complexity):
             self.all_features = False
             self.complexity += 1
         elif self.active_problems_solved and any(not solved for solved in self.solved.values()):
             self.all_features = False
             self.max_cost = MAX_COST
             self.active_problems_solved = False
+            self.complexity = self.succ_complexity
             next_problem = next(problem for problem in self.problems if not self.solved[problem.name])
             if self.active_problems and self.problems.index(next_problem) > max(
                 [self.problems.index(problem) for problem in self.active_problems]):
