0d8dd4357274da90d5778ebc9dcccec4a8921dda
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2025-03-08 10:20:44 -0500

    Add type hints to the grounder

--- genfond/ground.py
+++ genfond/ground.py
@@ -1,47 +1,57 @@
 import itertools
+from typing import Optional
 
 from pddl.action import Action
+from pddl.core import Domain, Problem
+from pddl.custom_types import name as name_type
 from pddl.logic import Predicate, Variable
-from pddl.logic.base import And, BinaryOp, UnaryOp
+from pddl.logic.base import And, BinaryOp, Formula, UnaryOp
 from pddl.logic.effects import When
 from pddl.logic.functions import BinaryFunction, NumericFunction, NumericValue
 from pddl.logic.predicates import EqualTo
-from pddl.logic.terms import Constant
+from pddl.logic.terms import Constant, Term
 
+type TypeTag = Optional[name_type]
 
-def _ground_ops(op, mapping):
-    optype = type(op)
-    if optype == Constant:
-        return op
-    if optype == Predicate:
-        return Predicate(op.name, *[_ground_ops(t, mapping) for t in op.terms])
-    elif optype == Variable:
+def _ground_term(term: Term, mapping: dict[Variable, Constant]) -> Constant:
+    term_type = type(term)
+    if isinstance(term, Constant):
+        return term
+    elif isinstance(term, Variable):
         try:
-            return mapping[op]
+            return mapping[term]
         except KeyError as e:
             raise NameError(
-                f'Unknown variable {op}, known variables: {", ".join([str(k) for k in mapping.keys()])}') from e
+                f'Unknown variable {term}, known variables: {", ".join([str(k) for k in mapping.keys()])}') from e
+    else:
+        raise TypeError(f"{term}: unknown term type: {term_type}")
+
+
+def _ground_formula(op: Formula, mapping: dict[Variable, Constant]) -> Formula:
+    optype = type(op)
+    if optype == Predicate:
+        return Predicate(op.name, *[_ground_term(t, mapping) for t in op.terms])
     elif optype == EqualTo:
-        return EqualTo(_ground_ops(op.left, mapping), _ground_ops(op.right, mapping))
+        return EqualTo(_ground_term(op.left, mapping), _ground_term(op.right, mapping))
     elif issubclass(optype, UnaryOp):
-        return optype(_ground_ops(op.argument, mapping))
+        return optype(_ground_formula(op.argument, mapping))
     elif issubclass(optype, BinaryOp):
-        return optype(*[_ground_ops(t, mapping) for t in op.operands])
+        return optype(*[_ground_formula(t, mapping) for t in op.operands])
     elif optype == And:
-        return optype(*[_ground_ops(t, mapping) for t in op.operands])
+        return optype(*[_ground_formula(t, mapping) for t in op.operands])
     elif optype == When:
-        return optype(_ground_ops(op.condition, mapping), _ground_ops(op.effect, mapping))
+        return optype(_ground_formula(op.condition, mapping), _ground_formula(op.effect, mapping))
     elif issubclass(optype, BinaryFunction):
-        return optype(*[_ground_ops(t, mapping) for t in op.operands])
+        return optype(*[_ground_formula(t, mapping) for t in op.operands])
     elif optype == NumericValue:
         return op
     elif issubclass(optype, NumericFunction):
-        return optype(op.name, *[_ground_ops(t, mapping) for t in op.terms])
+        return optype(op.name, *[_ground_term(t, mapping) for t in op.terms])
     else:
-        raise TypeError("Unknown operator type: {}".format(optype))
+        raise TypeError(f"{op}: unknown operator type: {optype}")
 
 
-def _is_subtype(subtype, supertype, type_dict):
+def _is_subtype(subtype: TypeTag, supertype: TypeTag, type_dict: dict[TypeTag, TypeTag]) -> bool:
     if subtype == supertype:
         return True
     if subtype in type_dict:
@@ -49,36 +59,36 @@ def _is_subtype(subtype, supertype, type_dict):
     return False
 
 
-def _check_types(constant, variable, type_dict):
+def _check_types(constant: Constant, variable: Variable, type_dict: dict[TypeTag, TypeTag]) -> bool:
     if not type_dict:
         # There are no types, so we assume everything is of the same type.
         return True
     return any(_is_subtype(constant.type_tag, v_type, type_dict) for v_type in variable.type_tags)
 
 
-def ground_action(domain, problem, action, grounding):
+def ground_action(domain: Domain, action, grounding: tuple[Constant]) -> Optional[Action]:
     if not isinstance(action, Action):
         action = [a for a in domain.actions if a.name == action][0]
     mapping = dict(zip(action.parameters, grounding))
     if not all(_check_types(c, v, domain.types) for v, c in mapping.items()):
         return None
-    ground_precondition = _ground_ops(action.precondition, mapping)
-    ground_effect = _ground_ops(action.effect, mapping)
+    ground_precondition = _ground_formula(action.precondition, mapping)
+    ground_effect = _ground_formula(action.effect, mapping)
     return Action(action.name, parameters=grounding, precondition=ground_precondition, effect=ground_effect)
 
 
-def ground(domain, problem):
+def ground(domain: Domain, problem: Problem) -> list[Action]:
     constants = domain.constants | problem.objects
     operators = []
     for action in domain.actions:
         for grounding in itertools.product(constants, repeat=len(action.parameters)):
-            op = ground_action(domain, problem, action, grounding)
+            op = ground_action(domain, action, grounding)
             if op:
                 operators.append(op)
     return sorted(operators, key=lambda a: (a.name, a.parameters))
 
 
-def ground_domain_predicates(domain, problem):
+def ground_domain_predicates(domain: Domain, problem: Problem) -> set[Predicate]:
     constants = domain.constants | problem.objects
     ground_predicates = set()
     for predicate in domain.predicates:
@@ -86,6 +96,6 @@ def ground_domain_predicates(domain, problem):
             mapping = dict(zip(predicate.terms, grounding))
             if not all(_check_types(c, v, domain.types) for v, c in mapping.items()):
                 continue
-            ground_predicate = _ground_ops(predicate, mapping)
+            ground_predicate = _ground_formula(predicate, mapping)
             ground_predicates.add(ground_predicate)
     return ground_predicates
